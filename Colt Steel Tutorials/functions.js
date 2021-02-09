f83616a19b997732092d29c3adceabd9eb77e3e5
ezikechris <christophermbe1009@gmail.com>
2021-02-09 15:38:20 +0100

    added Colt Steel Tutorials

---
+++ Colt Steel Tutorials/functions.js
@@ -0,0 +1,36 @@
+//// JAVASCRIPT FUNCTIONS /////
+
+
+
+// functions also enambs us to prevent repeating our codes that is dont repaet your code///////
+var = function singSong() {
+ if (singSong === password) {
+     console.log('Your Connected')
+ }
+}
+
+
+
+//// FUNCTIONS ARGUMENTS OR PARAMETERS///
+
+
+function displayGreeting(name) {
+    const message = 'Hello' + 'name'
+    console.log(message);
+}
+
+displayGreeting('christopher');
+
+
+
+
+function displayGreeting(name, salutation='Hello') {
+    console.log('${salutation}, ${name}' );
+}
+
+
+/// HIGHER ORDER FUNCTION ///
+
+
+// A FUNCTION WHICH WE CAN PASS ANOTHER FUNCTION TO IT E.G SETINTERVAL() AS AN ARGUMENT OR PARAMETER///
+
